cb1d1885622b3adf1ad611256c59d770832103d8
Kaan Tekiner <kaantekiner96@gmail.com>
2019-02-09 19:28:36 +0300

    Update hardwareUsage.py

--- hardwareUsage.py
+++ hardwareUsage.py
@@ -15,43 +15,63 @@ def convertBytestoReadable(n):
             return '%.1f%s' % (value, s)
     return "%sB" % n
 
-cpufamily = platform.processor()
-cpuname = cpuinfo.get_cpu_info()['brand']
-print("\n")
-print("--CPU--")
-print("Cpu Family =", cpufamily)
-print("Cpu Name =", cpuname)
-print("Core Count =", psutil.cpu_count())
-cpu = psutil.cpu_percent(interval=1)
-print("Percentage =",str(cpu) + "%")
-psutil.virtual_memory()
-ram = psutil.virtual_memory()
-print("--RAM--")
-print("Total =",ram.total >> 20,"mb")
-print("Available =",ram.available >> 20,"mb")
-print("Percentage =", str(ram.percent) + "%")
-print("Used =",ram.used >> 20,"mb")
-print("Free =",ram.free >> 20,"mb")
-print("--DISK--")
-print("MOUNTED")
-disk = psutil.disk_partitions(all=False)
-for item in disk:
-    if item.fstype == "" or item.fstype is None:
-        continue
-    else:
-        print("----")
-        print("Device =", item.device)
-        print("Mount Point =", item.mountpoint)
-        print("fstype =", item.fstype)
-        print("opts =", item.opts)
-        print("Usage;",)
-        usagelist = psutil.disk_usage(item.device)
-        print("Total =", convertBytestoReadable(usagelist.total))
-        print("Used =", convertBytestoReadable(usagelist.used))
-        print("Free =", convertBytestoReadable(usagelist.free))
-        print("Percent =", str(usagelist.percent) + "%")
-print("----")
-print("UNMOUNTED")
-for item in disk:
-    if item.fstype == "" or item.fstype is None:
-        print(item.device)
+def Start():
+    cpufamily = platform.processor()
+    cpugeneral = cpuinfo.get_cpu_info()
+    cpuname = cpugeneral['brand']
+    cpuarch = cpugeneral['arch']
+    cpubits = cpugeneral['bits']
+    cpuraw_arch_string = cpugeneral['raw_arch_string']
+    cpuhz_advertised = cpugeneral['hz_advertised']
+    cpuhz_actual = cpugeneral['hz_actual']
+    cpul2_cache_size = cpugeneral['l2_cache_size']
+    print("\n")
+    print("--CPU--")
+    print("Cpu Family =", cpufamily)
+    print("Cpu Name =", cpuname)
+    print("Core Count =", psutil.cpu_count())
+    print("Arch =", cpuarch)
+    print("Bits =", cpubits)
+    print("Arch String =", cpuraw_arch_string)
+    print("Known Hz =", cpuhz_advertised)
+    print("Actual Hz =", cpuhz_actual)
+    print("Cache Size =", cpul2_cache_size)
+    cpu = psutil.cpu_percent(interval=1)
+    print("Percentage =",str(cpu) + "%")
+    psutil.virtual_memory()
+    ram = psutil.virtual_memory()
+    print("--RAM--")
+    print("Total =",ram.total >> 20,"mb")
+    print("Available =",ram.available >> 20,"mb")
+    print("Percentage =", str(ram.percent) + "%")
+    print("Used =",ram.used >> 20,"mb")
+    print("Free =",ram.free >> 20,"mb")
+    print("--DISK--")
+    print("MOUNTED")
+    disk = psutil.disk_partitions(all=False)
+    for item in disk:
+        if item.fstype == "" or item.fstype is None:
+            continue
+        else:
+            print("----")
+            print("Device =", item.device)
+            print("Mount Point =", item.mountpoint)
+            print("fstype =", item.fstype)
+            print("opts =", item.opts)
+            print("Usage;",)
+            usagelist = psutil.disk_usage(item.device)
+            print("Total =", convertBytestoReadable(usagelist.total))
+            print("Used =", convertBytestoReadable(usagelist.used))
+            print("Free =", convertBytestoReadable(usagelist.free))
+            print("Percent =", str(usagelist.percent) + "%")
+    print("----")
+    print("UNMOUNTED")
+    for item in disk:
+        if item.fstype == "" or item.fstype is None:
+            print(item.device)
+
+
+if __name__ == '__main__':
+    Start()
+
+    
